9ada22b4c05849865aee07939520dc28dffe6987
sonofvanu <yashyajnesh@gmail.com>
2017-09-12 22:59:40 +0530

    hello

--- Step6-Assignment/src/com/stackroute/datamunger/DataMunger.java
+++ Step6-Assignment/src/com/stackroute/datamunger/DataMunger.java
@@ -2,14 +2,13 @@ package com.stackroute.datamunger;
 
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
+import java.util.stream.Collectors;
 
 import com.stackroute.datamunger.query.DataSet;
 import com.stackroute.datamunger.query.DataTypeDefinitions;
 import com.stackroute.datamunger.query.Filter;
+import com.stackroute.datamunger.query.GenericComparator;
 import com.stackroute.datamunger.query.Header;
 import com.stackroute.datamunger.query.Query;
 import com.stackroute.datamunger.query.Row;
@@ -30,11 +29,13 @@ public class DataMunger {
 		
 		
 		//call executeQuery() method to get the resultSet and write to JSON file
-		String queryString = "select city,winner,team1,team2,player_of_match from data/ipl.csv where season >= 2008 and city = bangalore";
+		String queryString = "select city,winner,team1,team2,player_of_match from data/ipl.csv where season >= 2013 or toss_decision != bat and city = Bangalore group by team1 order by win_by_runs";
 		QueryParameter queryParameter;
 		QueryParser qp = new QueryParser();
 		queryParameter = qp.parseQuery(queryString);
 		DataSet dataSet = new DataSet();
+		HashMap<Long,String> orderingData=new HashMap<>();
+		DataSet orderByDataSet=new DataSet();
 		Filter filter = new Filter();
 		BufferedReader bufferedReader;
 		try {
@@ -71,11 +72,14 @@ public class DataMunger {
 			int tempheaderCollectionSize = tempHeaderCollection.size();
 			String[] singleRowData;
 			dataSet.clear();
+			orderByDataSet.clear();
+			orderingData.clear();
 			if (queryParameter.getFields().get(0).equals("*")) {
 				if (!queryParameter.getRestrictions().isEmpty()) {
 					singleRow = bufferedReader.readLine();
 					while (singleRow != null) {
 						Row row = new Row();
+						Row orderBy=new Row();
 						row.clear();
 						singleRowData = singleRow.split(",");
 						int indexPoint = 0;
@@ -85,6 +89,8 @@ public class DataMunger {
 								indexPoint++;
 
 								if (indexPoint == tempheaderCollectionSize - 1) {
+									orderBy.put(queryParameter.getOrderByFields().get(0), singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
+									orderingData.put(locationPoint, singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
 									break;
 								}
 							} /*
@@ -93,27 +99,49 @@ public class DataMunger {
 
 						}
 						dataSet.put(locationPoint, row);
+						orderByDataSet.put(locationPoint, orderBy);
 						locationPoint++;
 						singleRow = bufferedReader.readLine();
+					}
+					
+					if(!queryParameter.getOrderByFields().isEmpty())
+					{
+						System.out.println("hello1");
 					}
 				} else {
 					bufferedReader.readLine();
 					while (singleRow != null) {
 						Row row = new Row();
+						Row orderBy=new Row();
+						orderBy.clear();
 						row.clear();
+					
 						singleRowData = singleRow.split(",");
 						int indexPoint = 0;
 						while (indexPoint < tempheaderCollectionSize) {
 							row.put(Header[indexPoint], singleRowData[indexPoint]);
 							indexPoint++;
 							if (indexPoint == tempheaderCollectionSize - 1) {
+								orderBy.put(queryParameter.getOrderByFields().get(0), singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
+								orderingData.put(locationPoint, singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
 								break;
 							}
 						}
 						dataSet.put(locationPoint, row);
+						orderByDataSet.put(locationPoint, orderBy);
 						locationPoint++;
 						singleRow = bufferedReader.readLine();
 					}
+					
+					if(!queryParameter.getOrderByFields().isEmpty())
+					{
+						System.out.println("hello2");
+					}
+					
+					
+					
+					
+					
 				}
 			} else {
 				if (!queryParameter.getRestrictions().isEmpty()) {
@@ -121,12 +149,13 @@ public class DataMunger {
 					
 					while (singleRow != null) {
 						Row row = new Row();
+						Row orderBy=new Row();
+						orderBy.clear();
 						row.clear();
 						singleRowData = singleRow.split(",");
 						int indexPoint = 1;
 						
 							if (filter.expressionEvaluator(singleRow, header, rowDataTypeDefinitions, queryParameter)) {
-						System.out.println(filter.expressionEvaluator(singleRow, header, rowDataTypeDefinitions, queryParameter)+"lol........"+locationPoint);
 								for (String eachColumn : queryParameter.getFields()) {
 									row.put(eachColumn, singleRowData[header.get(eachColumn)]);
 								}
@@ -134,28 +163,42 @@ public class DataMunger {
 								/*if (indexPoint == tempheaderCollectionSize - 1) {
 									break;
 								}*/
-
+								orderBy.put(queryParameter.getOrderByFields().get(0), singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
+								orderingData.put(locationPoint, singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
 							} 
 
 						
 						dataSet.put(locationPoint, row);
+						orderByDataSet.put(locationPoint, orderBy);
 						locationPoint++;
 						singleRow = bufferedReader.readLine();
 						
 					}
+					System.out.println("ola");
+					
+					
 
 				} else {
 					bufferedReader.readLine();
 					while (singleRow != null) {
 						Row row = new Row();
+						Row orderBy=new Row();
+						orderBy.clear();
 						row.clear();
 						singleRowData = singleRow.split(",");
 						for (String eachColumn : queryParameter.getFields()) {
 							row.put(eachColumn, singleRowData[header.get(eachColumn)]);
 						}
+						orderBy.put(queryParameter.getOrderByFields().get(0), singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
+						orderingData.put(locationPoint, singleRowData[header.get(queryParameter.getOrderByFields().get(0))]);
 						dataSet.put(locationPoint, row);
+						orderByDataSet.put(locationPoint, orderBy);
 						locationPoint++;
 						singleRow = bufferedReader.readLine();
+					}
+					if(!queryParameter.getOrderByFields().isEmpty())
+					{
+						System.out.println("hello4");
 					}
 				}
 			}
@@ -167,17 +210,60 @@ public class DataMunger {
 			{
 				dataSet.remove(i);
 			}
+			if(orderByDataSet.get(i).isEmpty())
+			{
+				orderByDataSet.remove(i);
+			}
+		}
+		
+	/*	if(!queryParameter.getOrderByFields().isEmpty())
+		{
+			List<Row> afterOrdered=new ArrayList<>(dataSet.values());
+			List finalOrder=new ArrayList<>();
+			GenericComparator genericComparator=new GenericComparator();
+			genericComparator.setSortingIndex(queryParameter.getOrderByFields().get(0).toLowerCase());
+			//Collections.sort(afterOrdered,(Comparator<? super Row>) finalOrder);
+			System.out.println("afterordered");
+			//afterOrdered.forEach(System.out::println);
+			
+			System.out.println(afterOrdered.get(0).get("team1"));
+		}*/
+	//	System.out.println("after final"+dataSet);
+		
+		
+		
+		
+		List<Row> list=new ArrayList(orderByDataSet.values());
+		List<Row> listempty=new ArrayList<>();
+		listempty=list;
+		List locationList=new ArrayList<>(orderByDataSet.entrySet());
+		System.out.println(list.size());
+		if(!queryParameter.getOrderByFields().isEmpty())
+		{
+			GenericComparator genericComparator=new GenericComparator();
+			genericComparator.setSortingIndex(queryParameter.getOrderByFields().get(0));
+			Collections.sort(list,genericComparator);
+			
+		}
+		DataSet ds=new DataSet();
+		List<Long> orderedLocation=new ArrayList<>();
+		long sizeOfList=(long) list.size();
+		for(int i=0;i<size;i++)
+		{
+			for(int j=0;j<size;j++)
+			{
+				if(list.get(i).toString().equals(listempty.get(j).toString()))
+				{
+					System.out.println("hi");
+				}
+			}
+			
 		}
 		
 		
-		
-		System.out.println("after final"+dataSet);
-		
-		System.out.println("lololo");
-		
-		List bag=new ArrayList<>(dataSet.values());
-		bag.forEach(System.out::println);
-		
+
+		System.out.println("hehehe");
+		orderedLocation.forEach(System.out::println);
 		
 		
 	}
